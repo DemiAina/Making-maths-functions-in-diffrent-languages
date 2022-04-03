9ae98f20d5cf3c6a6b4ff326b8e5d1afb2567a43
unknown <demiainasjf@gmail.com>
2022-04-03 12:24:24 +0100

    optimization made to scanf

--- C/maths.c
+++ C/maths.c
@@ -5,8 +5,9 @@ int main(void){
 
     int num1,num2;
     printf ("Enter 2 numbers to be added : ");
-    scanf("%d", &num1);
-    scanf("%d",&num2);
+    
+    scanf("%d%d", &num1,&num2);
+    
     printf("\nThe numbers you want added is %d and %d\n", num1,num2);
     
     int val = add(num1 , num2);
