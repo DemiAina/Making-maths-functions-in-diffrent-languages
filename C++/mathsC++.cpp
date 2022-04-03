62c733766f3880edee8621c62b293dc71dafab55
unknown <demiainasjf@gmail.com>
2022-04-03 13:01:02 +0100

    added maths.cpp

---
+++ C++/mathsC++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+
+    int num1,num2;
+
+    cout << "Please enter 2 numbers to add" << endl;
+
+    cin >> num1; cin >> num2;
+
+    int val = num1 + num2;
+
+    cout << num1 << " + " << num2 << " = " << val << endl;
+
+
+}
