7b75bc393fd63426e7fe982559dc7d49066904b6
unknown <demiainasjf@gmail.com>
2022-04-03 14:26:22 +0100

    javascript maths code made and updated files sturture

---
+++ Javascript/maths.js
@@ -0,0 +1,18 @@
+function add(a,b){
+ 
+    return a + b;
+}
+
+"use strict";
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+console.log("Please enter two numbers to add");
+
+var num1 = parseInt(prompt("Enter number 1 to add : "));
+var num2 = parseInt(prompt("Enter the second number to add : "));
+
+var val = add(num1, num2);
+
+console.log(`${num1} + ${num2} = ${val}`);
